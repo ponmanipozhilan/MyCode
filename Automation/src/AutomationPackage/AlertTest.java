55ace2b909e8f73baae879cc8fe87bded65e9ef9
ponmanipozhilan <ponmanipozhilan@gmail.com>
2016-12-14 20:16:07 +0530

    AlertTest

---
+++ Automation/src/AutomationPackage/AlertTest.java
@@ -0,0 +1,84 @@
+package AutomationPackage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class AlertTest {
+
+	@Test
+	public void AlertTesting() throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://atqa.testqa-lcbs.honeywell.com");
+		
+		driver.findElement(By.id("cred_userid_inputtext")).sendKeys("JSmith1@atqa.lcbsconnect.com");
+		Thread.sleep(2000);
+		driver.findElement(By.id("cred_password_inputtext")).sendKeys("lcbs@123");
+		Thread.sleep(2000);
+		driver.findElement(By.id("cred_sign_in_button")).click();
+		Thread.sleep(15000);
+		driver.findElement(By.xpath("//*[@id='main-nav']/ul/li[2]/a")).click();
+		Thread.sleep(10000);
+
+		driver.findElement(By.xpath("//*[@id='main-content']/ul/li/div[1]/a/article/span[contains(text(), 'Shinola South store')]")).click();
+		Thread.sleep(3000);
+
+		driver.findElement(By.xpath(".//*[@id='alert-config-nav']/a[3]")).click();
+		Thread.sleep(5000);
+				
+		driver.findElement(By.xpath("//*[@id='alert-settings-sort']/a")).click();
+		Thread.sleep(10000);
+
+		driver.findElement(By.xpath("//*[@id='add-alert-container']/section/div[1]/div/a")).click();
+		Thread.sleep(3000);
+
+		driver.findElement(By.xpath("//*[@id='add-alert-container']/section/div[1]/div/ul/li/a[contains(text(), 'RTU_101')]")).click();
+
+		driver.findElement(By.xpath("//*[@id='add-alert-container']/section/div[2]/div/a")).click();
+		Thread.sleep(5000);
+
+		driver.findElement(By.xpath("//*[@id='add-alert-container']/section/div[2]/div/ul/li/a[contains(text(), 'Digital')]")).click();
+
+
+		driver.findElement(By.xpath("//*[@id='add-alert-container']/section/div[3]/input")).sendKeys("Test");
+
+		driver.findElement(By.xpath("//*[@id='add-alert-container']/section/div[4]/div/a")).click();
+		Thread.sleep(3000);
+
+		driver.findElement(By.xpath("//*[@id='add-alert-container']/section/div[4]/div/ul/li/a[contains(text(), 'High')]")).click();
+
+		driver.findElement(By.xpath("//*[@id='add-alert-container']/section/div[5]/div/a")).click();
+		Thread.sleep(2000);
+
+		driver.findElement(By.xpath("//*[@id='add-alert-container']/section/div[5]/div/ul/li/a[contains(text(), 'Dirty Filter')]")).click();
+
+		driver.findElement(By.xpath("//*[@id='add-alert-container']/section/div[6]/input")).clear();
+		driver.findElement(By.xpath("//*[@id='add-alert-container']/section/div[6]/input")).sendKeys("2");
+
+		driver.findElement(By.xpath("//*[@id='add-alert-container']/section/div[7]/div/div")).click();
+		Thread.sleep(2000);
+
+		driver.findElement(By.xpath("//*[@id='add-alert-container']/section/div[7]/div/ul/li/a[contains(text(), '0-Normally Closed')]")).click();
+		
+		driver.findElement(By.xpath("//*[@id='add-alert-container']/section/div[10]/textarea")).sendKeys("Test");
+		Thread.sleep(2000);
+
+		driver.findElement(By.xpath("//*[@id='add-alert-container']/section/section/button")).click();
+		
+		Thread.sleep(30000);
+		String AA = driver.findElement(By.xpath("//*[@id='alert-settings-list']/ul/li/div/div/article/dl[1]/dt/a[contains(text(), 'ponmani0')]")).getText();
+		
+		Assert.assertEquals("ponmani0", AA);
+		
+		
+	}
+
+}
