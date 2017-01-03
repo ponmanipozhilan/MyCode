488a4125edc26165f5076cbb39931901d0bdb1f2
ponmanipozhilan <ponmanipozhilan@gmail.com>
2017-01-03 11:44:26 +0530

    Find element change

--- Automation/src/AutomationPackage/TestDataAlert.java
+++ Automation/src/AutomationPackage/TestDataAlert.java
@@ -12,18 +12,17 @@ public class TestDataAlert {
 			Object[][] obj = new Object[1][11];
 			
 			
-			obj[0][0]="https://atqa.testqa-lcbs.honeywell.com";
-			obj[0][1]="JSmith1@atqa.lcbsconnect.com";
+			obj[0][0]="https://perfdev.testqa-lcbs.honeywell.com";
+			obj[0][1]="WNutjob5747@cbploaddev.onmicrosoft.com";
 			obj[0][2]="lcbs@123";
-			obj[0][3]="Shinola South Store 1253";
+			obj[0][3]="Borsheims6476";
 			obj[0][4]="RTU_1";
 			obj[0][5]="Digital";	
-			obj[0][6]="Create Alert7";
+			obj[0][6]="Create Alert2";
 			obj[0][7]="High";
 			obj[0][8]="Dirty Filter";
 			obj[0][9]="0-Normally Closed";
-			obj[0][10]="Edit Alert7";
-				
+			obj[0][10]="Edit Alert2";
 			
 			return obj;
 				
